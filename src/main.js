e6c4874d01ed58da023fb3918bf1229f22cd49b5
Joe Tagliaferro <jtagliaferro74@gmail.com>
2022-02-03 22:34:28 -0500

    cleaned up code. removed commented lines

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store/store.js";
-// import vueAwesomeCountdown from "vue-awesome-countdown";
-// import VueCountdown from "@chenfengyuan/vue-countdown";
 import VueCountdown from "@smithalan2/vue-countdown";
-// import VueConfetti from "vue-confetti";
 
 createApp(App).component(VueCountdown.name, VueCountdown).use(store).mount("#app");
